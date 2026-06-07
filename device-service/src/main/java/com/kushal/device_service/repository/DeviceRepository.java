86f3bea30418cd63d7c18446a938a4e5dc7a378a
kushalry <55235728+kushalry@users.noreply.github.com>
2026-06-07 15:28:13 +0530

    feat: Implement device management features with CRUD operations and database integration

---
+++ device-service/src/main/java/com/kushal/device_service/repository/DeviceRepository.java
@@ -0,0 +1,14 @@
+package com.kushal.device_service.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kushal.device_service.entity.Device;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+
+    List<Device> findAllByUserId(Long userId);
+}
